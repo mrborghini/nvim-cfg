df98654dc2a164ccde738cd32f1c284402d94c1a
mrborghini <61702082+mrborghini@users.noreply.github.com>
2024-09-04 22:01:09 +0200

    Added keybinds and luasnip

--- lua/nvimcfg/config.lua
+++ lua/nvimcfg/config.lua
@@ -1,8 +1,9 @@
 local set = vim.opt -- set options
+local keymap = vim.keymap
 
 set.tabstop = 4
 set.softtabstop = 4
 set.shiftwidth = 4
-
+keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 -- Set color of the terminal
 require("nvimcfg.color")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,8 +12,15 @@ return {
 	{ "hrsh7th/cmp-buffer" },    -- Buffer completions
 	{ "hrsh7th/cmp-path" },      -- Path completions
 	{ "hrsh7th/cmp-cmdline" },   -- Cmdline completions
+	{ "hrsh7th/vim-vsnip" }, -- Snippet
 	{ "saadparwaiz1/cmp_luasnip" }, -- Snippet completions
-	{ "L3MON4D3/LuaSnip" },      -- Snippet engine
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp"
+	}, -- Snippet engine
 
 	-- UI navigation
 	{ "nvim-tree/nvim-tree.lua" },
