f8b50b785de14150a58314b504eb1241be6fe431
mrborghini <61702082+mrborghini@users.noreply.github.com>
2025-10-18 19:45:11 +0200

    fix: remove deprecated language server

--- lua/cmpcfg.lua
+++ lua/cmpcfg.lua
@@ -1,27 +1,17 @@
--- Set up nvim-cmp.
 local cmp = require 'cmp'
 
 cmp.setup({
   snippet = {
-    -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+      vim.fn["vsnip#anonymous"](args.body)
     end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -43,22 +33,17 @@ cmp.setup({
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
+    { name = 'vsnip' },
   }, {
     { name = 'buffer' },
   })
 })
 
--- Set up lspconfig.
+-- LSP capabilities and on_attach
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Enable hover documentation
 local on_attach = function(_, bufnr)
   local opts = { noremap = true, silent = true }
-
-  -- Set keymap for manual hover documentation
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-
-  -- Automatically trigger hover on CursorHold
   vim.api.nvim_create_autocmd("CursorHold", {
     buffer = bufnr,
     callback = function()
@@ -67,51 +52,26 @@ local on_attach = function(_, bufnr)
   })
 end
 
-vim.opt.updatetime = 1000 -- Time in milliseconds (default is 4000)
+vim.opt.updatetime = 1000
 
--- Add 'on_attach' to each LSP setup
-require('lspconfig')['rust_analyzer'].setup {
-  capabilities = capabilities,
-  on_attach = on_attach
+-- Define all LSPs in new style
+local servers = {
+  'rust_analyzer',
+  'clangd',
+  'lua_ls',
+  'omnisharp',
+  'gopls',
+  'pyright',
+  'bashls',
+  'gdscript',
+  'svelte',
 }
 
-require('lspconfig')['clangd'].setup {
-  capabilities = capabilities,
-  on_attach = on_attach
-}
-
-require('lspconfig')['lua_ls'].setup {
-  capabilities = capabilities,
-  on_attach = on_attach
-}
-
-require('lspconfig')['omnisharp'].setup {
-  capabilities = capabilities,
-  cmd = { "omnisharp" },
-  on_attach = on_attach
-}
-
-require('lspconfig')['gopls'].setup {
-  capabilities = capabilities,
-  on_attach = on_attach
-}
-
-require('lspconfig')['pyright'].setup {
-  capabilities = capabilities,
-  on_attach = on_attach
-}
-
-require('lspconfig')['bashls'].setup {
-  capabilities = capabilities,
-  on_attach = on_attach
-}
-
-require('lspconfig')['gdscript'].setup {
-  capabilities = capabilities,
-  on_attach = on_attach
-}
-
-require('lspconfig')['svelte'].setup {
-  capabilities = capabilities,
-  on_attach = on_attach
-}
+for _, server in ipairs(servers) do
+  vim.lsp.config[server] = {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    cmd = server == "omnisharp" and { "omnisharp" } or nil,
+  }
+  vim.lsp.enable(server)
+end
